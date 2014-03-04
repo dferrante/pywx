b9ea4996a19aa3e94763eb7e48becb63e7c52508
Dan Ferrante <mach5@shuttercal.com>
2014-03-03 21:13:19 -0500

    up and running

---
+++ pywx/pythabot.py
@@ -0,0 +1,118 @@
+import sys
+import socket
+import string
+import time
+import logging as log
+log.basicConfig(level=log.DEBUG)
+
+
+class Pythabot:
+    def __init__(self,config):
+        self.config = config
+        self.buffer = ""
+        self.debounce = False
+        self.debounce2 = False
+        self.commands = {}
+        self.commandlist = []
+        self.sock = socket.socket()
+
+    def connect(self):
+        try:
+            self.sock.connect((self.config["host"],self.config["port"]))
+            log.info("Connected to %s" % self.config["host"])
+
+            if len(self.config["pass"]) != 0:
+                self.sendraw("PASS %s" % self.config["pass"])
+            else:
+                log.info("Account identification bypassed.")
+
+            self.sendraw("NICK %s" % self.config["nick"])
+            self.sendraw("USER %s %s bla :%s" % (self.config["ident"], self.config["host"], self.config["realname"]))
+            log.info("Identified as %s" % self.config["nick"])
+        except socket.error:
+            self.quit("Could not connect to port %s, on %s." % (self.config["port"] ,self.config["host"]))
+
+    def addCommand(self,text,func,permission,arglength):
+        self.commands[text] = {"func":func,"permission":permission,"arglength":arglength}
+        self.commandlist.append(text)
+
+    def initparse(self,line):
+        #[':techboy6601!~IceChat77@unaffiliated/techboy6601','PRIVMSG','#botters-test',':yo','wuts','up']
+        senderline = line[0]
+        chan = line[2]
+        msg = " ".join(line[3:])
+        msg = msg[1:]
+        args = msg.split(" ")
+        firstarg = args[0]
+        exapoint = senderline.find("!")
+        tildepoint = senderline.find("~") + 1
+        atpoint = senderline.find("@") + 1
+        sender = senderline[1:exapoint]
+        ident = senderline[tildepoint:atpoint-1]
+        mask = senderline[atpoint:]
+
+        parseinfo = {
+            "sender":sender,
+            "ident":ident,
+            "mask":mask,
+            "chan":chan,
+            "msg":msg,
+            "firstarg":firstarg,
+            "args":args
+            }
+        self.parse(parseinfo)
+
+    def parse(self, parseinfo):
+        msg = parseinfo["firstarg"]
+        if msg in self.commands:
+            log.info('got command %s %s' % (msg, parseinfo))
+            if self.commands[msg]["permission"] == "owner":
+                if parseinfo["mask"] == self.config["ownermask"]:
+                    self.commands[msg]["func"](parseinfo)
+
+            if self.commands[msg]["permission"] == "all":
+                self.commands[msg]["func"](parseinfo)
+
+    def listen(self):
+        try:
+            while 1:
+                self.buffer = self.buffer + self.sock.recv(1024)
+                log.debug(self.buffer)
+                if (("/MOTD" in self.buffer or 'End of message of the day' in self.buffer) and self.debounce == False):
+                    for chan in self.config["chans"]:
+                        self.sendraw("JOIN %s" % chan)
+                        log.info("Joined %s" % chan)
+                        self.debounce == True
+
+                temp = self.buffer.split("\n")
+                self.buffer = temp.pop()
+
+                for line in temp:
+                    line = string.rstrip(line)
+                    line = line.split(" ")
+
+                    if line[1] == "433":
+                        self.quit("Username '%s' is already in use! Aborting." % self.config["nick"])
+
+                    if line[0] == "PING":
+                        self.sendraw("PONG %s" % line[1])
+                        log.debug("PONG %s" % line[1])
+
+                    if line[1] == "PRIVMSG":
+                        self.initparse(line)
+        except socket.error:
+            log.error("Socket error. Reconnecting in 30s")
+            time.sleep(30)
+            self.connect()
+
+    def sendraw(self,msg):
+        self.sock.send(msg + "\r\n")
+
+    def privmsg(self,to,msg):
+        self.sock.send("PRIVMSG %s :%s\r\n" % (to, msg.encode('utf-8')))
+
+    def quit(self,errmsg):
+        log.error("%s" % errmsg)
+        self.sendraw("QUIT :%s" % self.config["quitmsg"])
+        self.sock.close()
+        sys.exit(1)
---
+++ pywx/runner.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#These commands were created to help with channel administration.
+#It includes mode change, self OP, raw message, channel joins,
+#parts, kicks, a prefix changer, and even a start timer!
+
+
+import pythabot
+import forecastio
+import functools
+import logging
+import collections
+import datetime
+import time
+import re
+
+from geopy.geocoders import GoogleV3
+geoloc = GoogleV3()
+
+fio_api_key = "0971c933da4dcd6e3fe4f01ccf62a90a"
+max_msg_len = 375
+
+
+def quit(parseinfo):
+    bot.quit("Quit")
+
+def catch_failure(func):
+    @functools.wraps(func)
+    def wrapper(parseinfo):
+        try:
+            func(parseinfo)
+        except Exception, e:
+            logging.exception("bot error")
+            bot.privmsg('mach5', str(e))
+    return wrapper
+
+def smart_print_return(func):
+    @functools.wraps(func)
+    def wrapper(parseinfo):
+        payload = func(parseinfo)
+        msg = []
+        for word in ' '.join(payload).split(' '):
+            msg.append(word)
+            if sum(map(len, msg)) > max_msg_len:
+                bot.privmsg(parseinfo['chan'], " ".join(msg[:-1]))
+                msg = [word]
+        bot.privmsg(parseinfo['chan'], " ".join(msg))
+    return wrapper
+
+latlong_re = re.compile(r'([0-9.-]+),([0-9.-]+)')
+
+def match_location(args):
+    args = " ".join(args)
+    name = ""
+    lat, lng = 0,0
+
+    #latlong
+    match = latlong_re.match(args)
+    if match:
+        name = "%s,%s" % (lat, lng)
+        lat, lng = match.groups()
+    else:
+        try:
+            loc = geoloc.geocode(args)
+            name = loc.address
+            lat = loc.latitude
+            lng = loc.longitude
+        except:
+            return None, None, None
+    return name, lat, lng
+
+cmap = {'black': '\x031','navy': '\x032','maroon': '\x035','green': '\x033','grey': '\x0314','royal': '\x0312','aqua': '\x0311',
+        'lime': '\x039','silver': '\x0315','orange': '\x037','pink': '\x0313','purple': '\x036','red': '\x034','teal': '\x0310',
+        'white': '\x030','yellow': '\x038'}
+icon_colors = {
+    'clear-day': 'white',
+    'clear-night': 'white',
+    'rain': 'aqua',
+    'snow': 'purple',
+    'sleet': 'pink',
+    'wind': 'royal',
+    'fog': 'silver',
+    'cloudy': 'silver',
+    'partly-cloudy-day': 'silver',
+    'partly-cloudy-night': 'silver',
+    'hail': 'pink',
+    'thunderstorm': 'red',
+    'tornado': 'red'
+}
+cc = lambda s,c: "%s%s%s" % (cmap[c], s, cmap['white']) if c != 'white' else s
+pht = lambda t,u='F',c='royal': cc("⇑%s°%s".decode('utf-8') % (int(t), u), c)
+plt = lambda t,u='F',c='navy': cc("⇓%s°%s".decode('utf-8') % (int(t), u), c)
+pt = lambda t,u='F',c='white': "%s %s°%s%s".decode('utf-8') % (cmap[c], int(t), u, cmap['white']) if c != 'white' else " %s°%s".decode('utf-8') % (int(t), u)
+ncc = lambda s: cc(s, 'orange')
+tcc = lambda s: cc(s, 'royal')
+icc = lambda s,i: cc(s, icon_colors[i])
+
+def color_strip(s):
+    for code in sorted(cmap.values(), key=lambda x: len(x), reverse=True):
+        s = re.sub(code, '', s)
+    return s
+
+unitobj = collections.namedtuple("UnitSet", 'wind, dist, temp, intensity, accum, press')
+def get_units(unitset):
+    if unitset == 'us':
+        return unitobj('mph', 'mi', 'F', 'in/hr', 'in', 'mbar')
+    if unitset == 'si':
+        return unitobj('m/s', 'km', 'C', 'mm/hr', 'cm', 'hPa')
+    if unitset == 'ca':
+        return unitobj('kph', 'km', 'C', 'mm/hr', 'cm', 'hPa')
+    if unitset == 'uk':
+        return unitobj('mph', 'km', 'C', 'mm/hr', 'cm', 'hPa')
+
+epoch_dt = lambda e: datetime.datetime.fromtimestamp(e)
+to_celcius = lambda f: (f-32)*5/9
+to_fahrenheight = lambda c: (c*9/5)+32
+wind_chill = lambda t, ws: int(35.74 + (0.6215*t) - 35.75*(ws**0.16) + 0.4275*t*(ws**0.16))
+wind_chill_si = lambda t, ws: int(13.12 + (0.6215*t) - 11.37*(ws**0.16) + 0.3965*t*(ws**0.16))
+wind_directions = [(11.25, 'N'),(33.75, 'NNE'),(56.25, 'NE'),(78.75, 'ENE'),(101.25, 'E'),(123.75, 'ESE'),
+                   (146.25, 'SE'),(168.75, 'SSE'),(191.25, 'S'),(213.75, 'SSW'),(236.25, 'SW'),(258.75, 'WSW'),
+                   (281.25, 'W'),(303.75, 'WNW'),(326.25, 'NW'),(348.75, 'NNW'),(360, 'N')]
+wind_direction = lambda bearing: [direction for deg, direction in wind_directions if deg >= bearing][0]
+
+
+def debug(parseinfo):
+    import ipdb
+    ipdb.set_trace()
+
+def colors(parseinfo):
+    bot.privmsg(parseinfo['chan'], " - ".join(["%s%s\x030" % (v,k) for k,v in cmap.iteritems()]))
+
+def wf(parseinfo):
+    args = parseinfo['args'][1:]
+    name, lat, lng = match_location(args)
+    if not name and not lat and not lng:
+        return ['No location matches found for: %s' % ' '.join(args),]
+    forecast = forecastio.load_forecast(fio_api_key, float(lat), float(lng))
+    units = get_units(forecast.json['flags']['units'])
+
+    payload = ['%s:' % ncc(name)]
+    for d in forecast.daily().data[:5]:
+        payload.append("%s: %s (%s/%s)".decode('utf-8') % (
+            cc(d.time.strftime('%a'), 'maroon'),
+            icc(d.summary, d.icon),
+            pht(d.temperatureMax, units.temp),
+            plt(d.temperatureMin, units.temp)
+        ))
+    return payload
+
+@catch_failure
+@smart_print_return
+def nwf(parseinfo):
+    return map(color_strip, wf(parseinfo))
+
+@catch_failure
+@smart_print_return
+def cwf(parseinfo):
+    return wf(parseinfo)
+
+def wx(parseinfo):
+    args = parseinfo['args'][1:]
+    name, lat, lng = match_location(args)
+    if not name and not lat and not lng:
+        return ['No location matches found for: %s' % ' '.join(args),]
+    forecast = forecastio.load_forecast(fio_api_key, float(lat), float(lng))
+    units = get_units(forecast.json['flags']['units'])
+    current = forecast.currently()
+
+    payload = []
+    payload.append('%s: %s%s' % (ncc(name), icc(current.summary, current.icon), pt(current.temperature, units.temp)))
+    if current.temperature < 50 and current.windspeed > 3 and units.temp == "F":
+        wc = wind_chill(current.temperature, current.windspeed)
+        payload.append('%s%s' % (tcc('Wind Chill:'), pt(wc, units.temp)))
+    if current.temperature < 10 and current.windspeed > 5 and units.temp == "C":
+        wc = wind_chill_si(current.temperature, current.windspeed)
+        payload.append('%s%s' % (tcc('Wind Chill:'), pt(wc, units.temp)))
+
+    payload.append('%s %s%s from %s' % (tcc('Winds:'), int(current.windspeed), units.wind, wind_direction(current.windbaring)))
+    payload.append('%s %s%%' % (tcc('Clouds:'), int(current.cloudcover)*100))
+    payload.append('%s%s' % (tcc('Dewpoint:'), pt(current.dewPoint, units.temp)))
+    payload.append('%s %s%%' % (tcc('Humidity:'), int(current.humidity*100)))
+    payload.append('%s %s %s' % (tcc('Pressure:'), int(current.pressure), units.press))
+
+    today = forecast.daily().data[0]
+    if today.sunriseTime and today.sunsetTime:
+        delta = today.sunsetTime - today.sunriseTime
+        daytime = '%sh%sm' % (delta.seconds/60/60, delta.seconds/60%60)
+        payload.append('%s ⇑%s ⇓%s %s'.decode('utf-8') % (tcc('Sun:'), today.sunriseTime.strftime('%I:%M%p').lower(),
+                                                          today.sunsetTime.strftime('%I:%M%p').lower(), daytime))
+
+    alerts = forecast.json['alerts'] if 'alerts' in forecast.json else None
+    if alerts:
+        payload.append('%s' % cc('Alerts:', 'red'))
+        payload.append(", ".join(['#%s: %s' % (c+1, cc(a['title'], 'orange')) for c, a in enumerate(alerts)]))
+
+    return payload
+
+@catch_failure
+@smart_print_return
+def nwx(parseinfo):
+    return map(color_strip, wx(parseinfo))
+
+@catch_failure
+@smart_print_return
+def cwx(parseinfo):
+    return wx(parseinfo)
+
+
+@catch_failure
+@smart_print_return
+def alerts(parseinfo):
+    args = parseinfo['args'][1:]
+    name, lat, lng = match_location(args)
+    forecast = forecastio.load_forecast(fio_api_key, float(lat), float(lng))
+
+    payload = []
+    payload.append('%s: ' % (ncc(name)))
+
+    #alerts
+    alerts = forecast.json['alerts'] if 'alerts' in forecast.json else None
+    if alerts:
+        payload.append('%s' % cc('Alerts:', 'red'))
+        payload.append(", ".join(['#%s: %s' % (c+1, cc(a['title'], 'orange')) for c, a in enumerate(alerts)]))
+
+    payload.append("Use 'alert # <location>' to retrieve alert text")
+
+    return payload
+
+@catch_failure
+def alert(parseinfo):
+    args = parseinfo['args'][2:]
+    alert_index = parseinfo['args'][1]
+    if '#' in alert_index:
+        alert_index = alert_index.lstrip('#')
+    alert_index = int(alert_index)
+
+    name, lat, lng = match_location(args)
+    forecast = forecastio.load_forecast(fio_api_key, float(lat), float(lng))
+
+    alerts = forecast.json['alerts'] if 'alerts' in forecast.json else None
+    if alerts:
+        alert = alerts[alert_index-1]
+        bot.privmsg(parseinfo['sender'], alert['title'])
+        bot.privmsg(parseinfo['sender'], alert['uri'])
+        for line in alert['description'].split('\n'):
+            bot.privmsg(parseinfo['sender'], str(line))
+        bot.privmsg(parseinfo['sender'], epoch_dt(alert['expires']).strftime('Expires: %Y-%m-%d %H:%M'))
+
+if __name__ == '__main__':
+    config = {
+        "host": "irc.slashnet.org",
+        "port": 6667,
+        "nick": "wx",
+        "ident": "wx",
+        "realname": "wx",
+        "pass": "",
+        "chans": ["#mefi"],
+        "admins": ["~mach5@cloak-FBE60E9A.hsd1.nj.comcast.net"],
+        "ownermask": "~mach5@cloak-FBE60E9A.hsd1.nj.comcast.net",
+        "quitmsg": "peace out"
+    }
+    #config = {
+        #"host": "irc.advance.net",
+        #"port": 6667,
+        #"nick": "wx",
+        #"ident": "wx",
+        #"realname": "wx",
+        #"pass": "aslchat",
+        #"chans": ["#oledevhaus"],
+        #"admins": ["~mach5@cloak-FBE60E9A.hsd1.nj.comcast.net"],
+        #"ownermask": "~mach5@cloak-FBE60E9A.hsd1.nj.comcast.net",
+        #"quitmsg": "peace out"
+    #}
+    bot = pythabot.Pythabot(config)
+
+    bot.addCommand("botquit",quit,"owner",1)
+    bot.addCommand("wf", cwf, "all", 2)
+    bot.addCommand("wx", cwx, "all", 2)
+    bot.addCommand("nwf", nwf, "all", 2)
+    bot.addCommand("nwx", nwx, "all", 2)
+    bot.addCommand("alerts", alerts, "all", 2)
+    bot.addCommand("alert", alert, "all", 2)
+    bot.addCommand("ipdb", debug, "all", 2)
+    bot.addCommand("colors", colors, "all", 2)
+    bot.connect()
+    bot.listen()
