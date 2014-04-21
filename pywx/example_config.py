f72384193c597d30ad9dee387c8bb5148987a6ae
Dan Ferrante <mach5@shuttercal.com>
2014-04-20 21:50:20 -0400

    added example config

---
+++ pywx/example_config.py
@@ -0,0 +1,14 @@
+config = {
+    "host": "irc.server.org",
+    "port": 6667,
+    "nick": "wx",
+    "ident": "wx",
+    "realname": "wx",
+    "pass": "",
+    "chans": ["#weather"],
+    "admins": ["~ident@hostname"],
+    "ownermask": "~ident@hostname",
+    "quitmsg": "peace out",
+    "redlink_user": '',
+    "redlink_pass": ''
+}
