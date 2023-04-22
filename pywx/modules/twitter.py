5eb8884181425ea002087bb8ff40b60cc6b02881
Dan Ferrante <dan.ferrante@gmail.com>
2023-04-21 21:04:23 -0400

    name and shame twitter blue

--- pywx/modules/twitter.py
+++ pywx/modules/twitter.py
@@ -25,9 +25,12 @@ class TwitterParser(ParserCommand):
                     twid = path.split('/')[3]
 
                     client = tweepy.Client(self.config["twitter_token"])
-                    data = client.get_tweet(twid, tweet_fields='entities')
+                    data = client.get_tweet(twid, tweet_fields='entities', user_fields='verified')
                     tweet = data.data.data
                     text = tweet['text']
+
+                    user_data = client.get_user(username=username, user_fields='verified')
+                    verified = user_data.data.data['verified']
 
                     for url in tweet.get('entities', {}).get('urls', []):
                         text = text.replace(url['url'], url['expanded_url'])
@@ -38,7 +41,8 @@ class TwitterParser(ParserCommand):
                             continue
                         if len(tweetlines) == 0:
                             username = irc_color(f'@{username}', 'orange', reset=True)
-                            tweetlines.append(f'{username}: {tweetline}')
+                            blue_loser = irc_color('[LOSER ALERT]', 'blue', reset=True) if verified else ''
+                            tweetlines.append(f'{username} {blue_loser}: {tweetline}')
                         else:
                             tweetlines.append(f'{tweetline}')
                     lines.extend(tweetlines)
