f922b51f9703dff55d8a2920d772acbc5c79e674
Dan Ferrante <dan.ferrante@gmail.com>
2023-10-27 18:33:36 -0400

    sort by id and make sure all get sent

--- pywx/modules/alerts.py
+++ pywx/modules/alerts.py
@@ -1,4 +1,3 @@
-import datetime
 import re
 from urllib.parse import quote_plus
 
@@ -17,11 +16,11 @@ def highlight(text, phrase):
 
 class Scanner(base.Command):
     multiline = True
-    template = """-------------
+    template = """{% for event in events %}-------------
         {{ datetime|c('royal') }} - {{ responding|c(station_color) }} - {{ event.id }}
         {% if full_address %} {{ full_address|c(vip_word_color) }} {% elif event['town'] %} {{ event['town']|c(vip_word_color) }} {% elif event['address'] %} {{ event['address']|c(vip_word_color) }} {% endif %}
         {% if full_address %} {{ gmaps_url }} {% endif %}
-        {{ transcription|highlight(event['symptom']) }}"""
+        {{ transcription|highlight(event['symptom']) }}{% endfor %}"""
 
     important_stations = ['45fire', '46fire', 'sbes']
     very_important_words = ['studer', 'sunrise', 'austin hill', 'foundations', 'apollo', 'foxfire', 'river bend', 'grayrock', 'greyrock', 'beaver', 'lower west']
@@ -46,37 +45,39 @@ class Scanner(base.Command):
             return text[index + len(town):].lstrip(',').lstrip('.').strip()
         return text
 
-    def event_context(self, event):
-        time = event['datetime'].strftime('%-I:%M%p')
-        responding = ' - '.join([unit for unit in event['responding'].split(',')])
-        station_color = 'red' if any([station in event['responding'].lower() for station in self.important_stations]) else 'orange'
-        vip_word_color = 'yellow' if any([word in event['transcription'].lower() for word in self.important_words]) else 'royal'
-        vip_word_color = 'red' if any([word in event['transcription'].lower() for word in self.very_important_words]) else vip_word_color
+    def event_context(self, events):
+        event_payloads = []
+        for event in events:
+            time = event['datetime'].strftime('%-I:%M%p')
+            responding = ' - '.join([unit for unit in event['responding'].split(',')])
+            station_color = 'red' if any([station in event['responding'].lower() for station in self.important_stations]) else 'orange'
+            vip_word_color = 'yellow' if any([word in event['transcription'].lower() for word in self.important_words]) else 'royal'
+            vip_word_color = 'red' if any([word in event['transcription'].lower() for word in self.very_important_words]) else vip_word_color
 
-        repeat_search = self.repeating_regex.search(event['transcription'])
-        if repeat_search:
-            first = self.townsplit(repeat_search.group('first'), event['town'])
-            repeat = self.townsplit(repeat_search.group('repeat'), event['town'])
-            transcription = '\n'.join([first, 'Repeating ' + repeat])
-        else:
-            transcription = self.townsplit(event['transcription'], event['town'])
+            repeat_search = self.repeating_regex.search(event['transcription'])
+            if repeat_search:
+                first = self.townsplit(repeat_search.group('first'), event['town'])
+                repeat = self.townsplit(repeat_search.group('repeat'), event['town'])
+                transcription = '\n'.join([first, 'Repeating ' + repeat])
+            else:
+                transcription = self.townsplit(event['transcription'], event['town'])
 
-        payload = {
-            'datetime': time,
-            'responding': responding,
-            'vip_word_color': vip_word_color,
-            'transcription': transcription,
-            'station_color': station_color,
-            'event': event,
-        }
+            payload = {
+                'datetime': time,
+                'responding': responding,
+                'vip_word_color': vip_word_color,
+                'transcription': transcription,
+                'station_color': station_color,
+                'event': event,
+            }
 
-        if event['address'] and event['town']:
-            full_address = f"{event['address']}, {event['town']}, NJ"
-            gmaps_url = f'https://www.google.com/maps/place/{quote_plus(full_address)}/data=!3m1!1e3'
-            payload['full_address'] = full_address
-            payload['gmaps_url'] = gmaps_url
-
-        return payload
+            if event['address'] and event['town']:
+                full_address = f"{event['address']}, {event['town']}, NJ"
+                gmaps_url = f'https://www.google.com/maps/place/{quote_plus(full_address)}/data=!3m1!1e3'
+                payload['full_address'] = full_address
+                payload['gmaps_url'] = gmaps_url
+            event_payloads.append(payload)
+        return {'events': event_payloads}
 
 
 @register_periodic(30, chans=['#scanner'])
@@ -85,16 +86,16 @@ class ScannerAlerter(Scanner):
         global LAST_ALERT
 
         if LAST_ALERT is None:
-            LAST_ALERT = datetime.datetime.now()
+            LAST_ALERT = self.event_table.find_one(order_by=['-id'])['id']
 
-        event = self.event_table.find_one(datetime={'gt': LAST_ALERT}, is_transcribed=True, order_by=['datetime'])
+        event = self.event_table.find(id={'gt': LAST_ALERT}, is_transcribed=True, order_by=['id'])
         if event:
-            LAST_ALERT = event['datetime']
+            LAST_ALERT = event['id']
             return self.event_context(event)
         raise base.NoMessage
 
 
-@register(commands=['lastalert',])
+@register(commands=['lastalert'])
 class LastScanner(Scanner):
     def parse_args(self, msg):
         parser = base.IRCArgumentParser()
@@ -109,4 +110,4 @@ class LastScanner(Scanner):
                 raise base.ArgumentError('Event not found')
         else:
             event = self.event_table.find_one(is_transcribed=True, order_by=['-datetime'])
-        return self.event_context(event)
+        return self.event_context([event])
