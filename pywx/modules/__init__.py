cf2125e8c3fef917a2e7da8a85985cfe35035ab4
Dan Ferrante <dan.ferrante@gmail.com>
2023-10-26 21:35:42 -0400

    cleaner way to do imports

--- pywx/modules/__init__.py
+++ pywx/modules/__init__.py
@@ -1,2 +1,7 @@
 import os
+import importlib
+
 __all__ = [os.path.splitext(f)[0] for f in os.listdir(os.path.split(__file__)[0]) if os.path.splitext(f)[1] == '.py' and os.path.split(f)[1] not in ['__init__.py',]]
+
+for module in __all__:
+    importlib.import_module(f'.{module}', package='modules')
--- pywx/pywx.py
+++ pywx/pywx.py
@@ -7,32 +7,23 @@ import os
 import sys
 
 from pythabot import Pythabot
-from modules import *
+from modules import registry
 
-parser = argparse.ArgumentParser()
-parser.add_argument("config_file", help="path to the config file")
-args = parser.parse_args()
-
-
-try:
-    config = json.load(open(args.config_file, encoding='utf-8'))
-    config['pywx_path'] = os.path.dirname(os.path.abspath(__file__))
-except ImportError:
-    print('cant import local_config.py')
-    sys.exit()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="path to the config file")
+    args = parser.parse_args()
+
+    try:
+        config = json.load(open(args.config_file, encoding='utf-8'))
+        config['pywx_path'] = os.path.dirname(os.path.abspath(__file__))
+    except ImportError:
+        print('cant import local_config.py')
+        sys.exit()
+
     reg = registry.registry
     reg.load_modules(config)
-
-    # print(reg.commands['lastquake'].run({'sender': 'bp', 'msg': 'lastquake', 'command': 'lastquake', 'args': '',}))
-    # parsed_things = []
-    # msg = {'msg': 'https://twitter.com/jbenton/status/1541888492443148293'}
-    # for parser in reg.parsers:
-    #     for line in parser.parse(msg):
-    #         parsed_things.append(line)
-    # for line in parsed_things:
-    #     print(line)
 
     bot = Pythabot(config, reg)
     bot.connect()
