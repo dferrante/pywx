6ac8e04469a0abeb892d5d7bfb47be83414af50b
Dan Ferrante <dan.ferrante@gmail.com>
2024-02-27 09:52:03 -0500

    corrections

--- add_correction.py
+++ add_correction.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
             exit()
         if not correction:
             correction = last_correction
-        corrections = spelling_correct.get(correction, set())
+        corrections = set(spelling_correct.get(correction, set()))
         corrections.add(misspelling)
         spelling_correct[correction] = set(corrections)
         for k, v in spelling_correct.items():
-            spelling_correct[k] = set(v)
+            spelling_correct[k] = sorted(list(set(v)))
         with open('spelling_correct.py', 'w') as f:
-            f.write(f'spelling_correct = {pprint.pformat(spelling_correct, compact=True)}\n')
+            f.write(f'spelling_correct = {pprint.pformat(spelling_correct, compact=True, indent=4)}\n')
         last_correction = correction
