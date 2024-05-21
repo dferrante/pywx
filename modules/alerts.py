f9df0459c9c38951b01e3b3134d16540f80a92d9
Dan Ferrante <dan.ferrante@gmail.com>
2024-05-21 14:12:06 -0400

    irc message tweaks

--- modules/alerts.py
+++ modules/alerts.py
@@ -18,8 +18,8 @@ class Scanner(base.Command):
     template = """-------------
         {{ datetime|c('royal') }} - {{ event['county']|c(county_color) }} - {{ responding|c(station_color) }} - {{ event.id }}
         {% if event['gmaps_address'] %} {{ event['gmaps_address']|c(vip_word_color) }} {% elif full_address %} {{ full_address|c(vip_word_color) }} {% elif event.town %} {{ event.town|c(vip_word_color) }} {% elif event.address %} {{ event.address|c(vip_word_color) }} {% endif %}
-        {% if event['gmaps_url'] %} {{ event['gmaps_url'] }} {% endif %}
-        {% if event['age'] %} {{ event['age'] }}yo {% endif %} {% if event['gpt_gender'] %} {{ event['gpt_gender'] }}, {% endif %} {% if event['gpt_incident_details'] %} {{ event['gpt_incident_details'] }} ({{ event['gpt_incident_type'] }}{% if event['gpt_incident_subtype'] %}-{{ event['gpt_incident_subtype'] }}{% endif %}){% endif %}"""
+        {{ scanner_url }}
+        {% if event['gpt_age'] or event['gpt_gender'] %} {% if event['gpt_age'] %} {{ event['gpt_age'] }}yo {% endif %} {% if event['gpt_gender'] %} {{ event['gpt_gender'] }} {% endif %} - {% endif %} {% if event['gpt_incident_details'] %} {{ event['gpt_incident_details'] }} ({{ event['gpt_incident_type'] }}{% if event['gpt_incident_subtype'] %}-{{ event['gpt_incident_subtype'] }}{% endif %}){% endif %}"""
 
     important_stations = ['45fire', '46fire', 'sbes', 'southbranch']
     very_important_words = ['studer', 'sunrise', 'austin hill', 'foundations', 'apollo', 'foxfire', 'river bend', 'grayrock', 'greyrock', 'beaver', 'lower west', 'norma']
@@ -64,6 +64,7 @@ class Scanner(base.Command):
             'station_color': station_color,
             'county_color': county_color,
             'event': event,
+            'scanner_url': f"https://{self.config['scanner_base_url']}/?id={event['id']}"
         }
 
         if event['address'] and event['town']:
