800789b86d12215a9f91a74951467b765e33fb8b
Dan Ferrante <dan.ferrante@gmail.com>
2024-12-07 13:43:34 -0500

    region control in the kml

--- dump_kml.py
+++ dump_kml.py
@@ -22,34 +22,59 @@ def dump_kml():
     event_table = database['scanner']
     #events that have gpt_latitude and gpt_longitude
     events = event_table.find(gmaps_latitude={'!=': None}, gmaps_longitude={'!=': None}, gmaps_location_type="ROOFTOP", order_by=['gpt_incident_type', 'gpt_incident_subtype'])
+    # events = list(events)
 
     # Create a KML object
     kml = simplekml.Kml()
 
+    # event_counter = collections.defaultdict(int)
+    # for event in events:
+    #     event_counter[(event['gmaps_longitude'], event['gmaps_latitude'])] += 1
+
     folders = {}
     for event in events:
-        if event['gpt_incident_type'] not in folders:
-            folders[event['gpt_incident_type']] = (kml.newfolder(name=event['gpt_incident_type']), {})
-        if event['gpt_incident_subtype'] not in folders[event['gpt_incident_type']][1]:
-            folders[event['gpt_incident_type']][1][event['gpt_incident_subtype']] = folders[event['gpt_incident_type']][0].newfolder(name=event['gpt_incident_subtype'])
+        incident_type = event['gpt_incident_type']
+        if not incident_type:
+            incident_type = 'unknown'
 
-        folder = folders[event['gpt_incident_type']][1][event['gpt_incident_subtype']]
+        incident_subtype = event['gpt_incident_subtype']
+        if not incident_subtype:
+            incident_subtype = 'unknown'
 
-        # Create a placemark
-        name = event['gpt_incident_subtype'] if event['gpt_incident_subtype'] else event['gpt_incident_type']
-        pnt = folder.newpoint(name=name, description=event['gpt_incident_details'], coords=[(event['gmaps_longitude'], event['gmaps_latitude'])])
+        if incident_type not in folders:
+            folders[incident_type] = (kml.newfolder(name=incident_type), {})
+        if incident_subtype not in folders[incident_type][1]:
+            folders[incident_type][1][incident_subtype] = folders[incident_type][0].newfolder(name=incident_subtype)
 
-        if event['gpt_incident_type'] == 'fire':
+        folder = folders[incident_type][1][incident_subtype]
+
+        name = incident_subtype if incident_subtype != 'unknown' else incident_type
+        pnt = folder.newpoint(
+            name=name,
+            description=event['gpt_incident_details'],
+            coords=[(event['gmaps_longitude'], event['gmaps_latitude'])],
+        )
+        pnt.timestamp.when = event['datetime'].isoformat()
+        latlonbox = simplekml.LatLonAltBox(
+            north=event['gmaps_latitude'] + 0.0005,
+            south=event['gmaps_latitude'] - 0.0005,
+            east=event['gmaps_longitude'] + 0.0005,
+            west=event['gmaps_longitude'] - 0.0005,
+            minaltitude=0,
+            maxaltitude=30000
+        )
+        pnt.region = simplekml.Region(latlonaltbox=latlonbox, lod=simplekml.Lod(minlodpixels=16, maxlodpixels=-1))
+
+        if incident_type == 'fire':
             icon = 'https://cdn-icons-png.flaticon.com/512/2769/2769523.png'
-        elif event['gpt_incident_type'] == 'medical' or event['gpt_incident_type'] == 'fall victim':
+        elif incident_type == 'medical' or incident_type == 'fall victim':
             icon = 'https://cdn-icons-png.flaticon.com/512/507/507579.png'
-        elif event['gpt_incident_type'] == 'accident':
+        elif incident_type == 'accident':
             icon = "https://cdn-icons-png.flaticon.com/512/2125/2125190.png"
         else:
             icon = 'https://maps.google.com/mapfiles/kml/shapes/placemark_circle_highlight.png'
         pnt.style.iconstyle.icon.href = icon
 
-    # Save the KML file
     kml.save('data/alerts.kml')
     database.close()
 
